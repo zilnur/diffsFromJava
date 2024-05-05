8dc4cce71c75eff75838c9f9dd3c071b3f88a39e
zilnur <86298777+zilnur@users.noreply.github.com>
2024-05-05 17:36:15 +0300

    add_modifications

--- src/main/kotlin/HumanPeople.kt
+++ src/main/kotlin/HumanPeople.kt
@@ -35,7 +35,11 @@ class HumanPeople {
     }
 
     private fun setSuffixAndPrint() {
-        val suffix = if (likes.toString().last() == '1') {"человеку"} else {"людям"}
+        val suffix = if (likes.toString().last() == '1' && likes.toString().takeLast(2) != "11") {
+            "человеку"
+        } else {
+            "людям"
+        }
         println("Понравилось $likes $suffix")
     }
 }
